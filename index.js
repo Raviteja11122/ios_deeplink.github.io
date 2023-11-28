04128de29c5cf267298383e2b30a76a3ec19b1c3
Raviteja Nomula <venkatasantoshraviteja.nomula@carrier.com>
2023-11-28 12:38:17 +0530

    test 3

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
       },
   
       openWebApp: function() {
-        //window.location.replace("https://apps.apple.com/us/app/ultrasync/id1103085533");
+        window.location.replace("https://apps.apple.com/us/app/ultrasync/id1103085533");
         //const iframe = document.getElementById("load_appstore");
         //iframe.setAttribute("src", 'https://itunes.apple.com/us/app/ultrasync/id1103085533');
         //iframe.setAttribute("style", 'display: block;');
