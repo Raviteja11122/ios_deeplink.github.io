b9c652c5468d1326fa4da8bfc100845babcb18da
Raviteja Nomula <venkatasantoshraviteja.nomula@carrier.com>
2024-06-24 16:16:12 +0530

    added ios and android links

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 (function() {
     var app = {
       launchApp: function() {
-        window.location.replace("POCNFCAPP://");
-        this.timer = setTimeout(this.openWebApp, 10000);
+        if(navigator.userAgent.toLowerCase().indexOf("android") > -1){
+          window.location.replace("https://dp.ultra-sync.com/ngms");
+          this.timer = setTimeout(this.openWebApp, 10000);
+        }
+        if(navigator.userAgent.toLowerCase().indexOf("iphone") > -1){
+          window.location.replace("com.aritech.ngms://main/ngms/");
+          this.timer = setTimeout(this.openWebApp, 10000);
+        }
       },
   
       openWebApp: function() {
-        window.location.href = "https://apps.apple.com/us/app/ultrasync/id1103085533";
+        if(navigator.userAgent.toLowerCase().indexOf("android") > -1){
+          window.location.href = "https://play.google.com/store/apps/details?id=com.uhssystems.ultrasyncplus";
+        }
+        if(navigator.userAgent.toLowerCase().indexOf("iphone") > -1){
+          window.location.href = "https://apps.apple.com/us/app/ultrasync/id1103085533";
+        }
+        
         //window.location.replace("https://apps.apple.com/us/app/ultrasync/id1103085533");
         //const iframe = document.getElementById("load_appstore");
         //iframe.setAttribute("src", 'https://itunes.apple.com/us/app/ultrasync/id1103085533');
